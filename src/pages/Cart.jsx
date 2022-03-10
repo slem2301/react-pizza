b508ab229a6d30ae0d4ba39ec59c8996cb015b3b
slem2301 <anton.vish2301@gmail.com>
2022-03-10 22:50:53 +0300

    three commit

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -124,7 +124,9 @@ const Cart = () => {
                             Для того, чтобы заказать пиццу, перейди на главную страницу.
                         </p>
                         <img src={emptyCart} alt="Empty cart" />
-                        
+                        <Link to="/" className="button button--black">
+                            <span>Вернуться dgtht</span>
+                        </Link>
                     </div>
             }
 
